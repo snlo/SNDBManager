3d5ace6ef3c002dbf1beb262ac34d2b27db1ed94
MrSunDong <snloveydus@sina.com>
2018-05-06 20:38:52 +0800

    add SNDBManager

---
+++ SNDBManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNDBManager
+//
+//  Created by sunDong on 2018/5/6.
+//  Copyright © 2018年 snloveydus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
