b50289a2d40b226ceb3b24d32131e3aa448fa8e4
snlo <snloveydus@sina.com>
2019-04-03 15:25:40 +0800

    update

--- SNDBManager/ViewController.m
+++ SNDBManager/ViewController.m
@@ -19,6 +19,8 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+//    pod trunk push SNDBManager.podspec --verbose --allow-warnings --use-libraries
 }
 
 
